5ab15b0cdf38aeeab303ae96bc23403458f910b1
Velid Duranovic <velid_provjera@outlook.com>
2023-01-15 14:47:05 +0100

    Updated header component to have two variations of it

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { enterFullScreenConst, exitFullScreenConst } from 'src/app/core/constants/header.constant';
+import { Router, NavigationEnd } from '@angular/router';
 import { images_folder_path } from 'src/environments/environment';
 
 @Component({
@@ -10,16 +11,24 @@ import { images_folder_path } from 'src/environments/environment';
 })
 export class HeaderComponent implements OnInit {
   public fullScreenIconPath?: string;
-  public fullScreen: any;
+  public fullScreen?: any;
+  public isGame?: boolean;
   
-  constructor() { }
+  constructor(private router:  Router, private ch: ChangeDetectorRef) { }
 
   public ngOnInit(): void {
+    this.router.events.subscribe((event) => (event instanceof NavigationEnd) && this.handleRouteChange());
+
     if(!document.fullscreenElement) {
       this.fullScreen = enterFullScreenConst;
     } else {
       this.fullScreen = exitFullScreenConst;
     }
+  }
+
+  public handleRouteChange(): void {
+    this.isGame = this.router.url.includes('game');
+    this.ch.detectChanges();
   }
 
   public toggleFullScreen(): void{
